b4c6a6c02e0df5e08d6eb6b3b2d2bef3922cfad7
geelario <udogabrieludo@gmail.com>
2020-12-15 14:23:40 +0100

    Updates

---
+++ precache-manifest.198026fb222d51b4bc4cf032e7505916.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ffd9b1259a0f0eda2fcd260cfaba6b",
+    "url": "/mymobilotto/index.html"
+  },
+  {
+    "revision": "059e5ed001a16192a6f5",
+    "url": "/mymobilotto/static/css/2.c1be056d.chunk.css"
+  },
+  {
+    "revision": "97994c42c4c0f1204cbf",
+    "url": "/mymobilotto/static/css/main.fc86860f.chunk.css"
+  },
+  {
+    "revision": "059e5ed001a16192a6f5",
+    "url": "/mymobilotto/static/js/2.a7c45a4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mymobilotto/static/js/2.a7c45a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97994c42c4c0f1204cbf",
+    "url": "/mymobilotto/static/js/main.23758528.chunk.js"
+  },
+  {
+    "revision": "274e6216401213083764",
+    "url": "/mymobilotto/static/js/runtime-main.9094f7d7.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/mymobilotto/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/mymobilotto/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/mymobilotto/static/media/revicons.2feb69cc.eot"
+  }
+]);
